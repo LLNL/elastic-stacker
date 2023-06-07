ebad34cee4280f6f1bc931b3b10340af36971ebd
James Taliaferro <taliaferro1@llnl.gov>
2023-06-06 23:01:14 -0700

    add an incomplete agent policy controller

--- tools/elastic_stacker/kibana/__init__.py
+++ tools/elastic_stacker/kibana/__init__.py
@@ -4,6 +4,7 @@ import httpx
 
 from .generic import GenericKibanaController
 from .saved_objects import SavedObjectController
+from .agent_policies import AgentPolicyController
 
 logger = logging.getLogger("elastic_stacker")
 
@@ -12,3 +13,4 @@ class KibanaController(GenericKibanaController):
     def __init__(self, client: httpx.Client):
         super().__init__(client)
         self.saved_objects = SavedObjectController(client=client)
+        self.agent_policies = AgentPolicyController(client=client)
---
+++ tools/elastic_stacker/kibana/agent_policies.py
@@ -0,0 +1,57 @@
+import logging
+import json
+import pathlib
+
+from slugify import slugify
+
+from .generic import GenericKibanaController
+
+logger = logging.getLogger("elastic_stacker")
+
+
+class AgentPolicyController(GenericKibanaController):
+    base_endpoint = "/api/fleet/agent_policies"
+    resource_directory = "agent_policies"
+
+    def get(
+        self,
+        perPage: int = None,
+        page: int = None,
+        kuery: str = None,
+        full: bool = None,
+        noAgentCount: bool = None,
+    ):
+        query_params = {
+            "perPage": perPage,
+            "page": page,
+            "full": full,
+            "noAgentCount": noAgentCount,
+            "kuery": kuery
+        }
+        query_params = self._clean_params(query_params)
+        response = self.client.get(
+            self.base_endpoint, params=query_params
+        )
+        return response.json()
+
+    # TODO
+    def create(self):
+        pass
+
+    # TODO
+    def load(self):
+        pass
+
+    def dump(
+        self,
+        output_directory: pathlib.Path,
+        include_managed: bool = False,
+    ):
+        agent_policies_directory = output_directory / self.resource_directory
+        agent_policies_directory.mkdir(exist_ok=True)
+        for policy in self._depaginate(self.get):
+            if include_managed or not policy["is_managed"]:
+                filename = slugify(policy["name"]) + ".json"
+                file_path = agent_policies_directory / filename
+                with file_path.open("w") as file:
+                    file.write(json.dumps(policy, indent=4, sort_keys=True))
