18f2f13058c36914b332e75e471f2703e8d3bf50
James Taliaferro <taliaferro1@llnl.gov>
2023-06-06 20:44:04 -0700

    add depaginate method, blacken

--- tools/elastic_stacker/elasticsearch/generic.py
+++ tools/elastic_stacker/elasticsearch/generic.py
@@ -3,8 +3,22 @@ import httpx
 
 class GenericElasticsearchController:
     client: httpx.Client
+
     def __init__(self, client: httpx.Client):
         self.client = client
+
+    def _depaginate(self, method, key, page_size=10):
+        """
+        Elasticsearch presents some of its APIs paginated, so rather than dump all of them
+        in one request we can turn that pagination into a nice, Pythonic generator.
+        """
+        offset = 0
+        results = {"count": float("inf")}
+        while offset < results["count"]:
+            results = method(offset=offset, size=page_size)
+            for result in results[key]:
+                offset += 1
+                yield result
 
     def _clean_params(self, params: dict):
         # httpx includes query parameters even if their value is None
@@ -12,4 +26,4 @@ class GenericElasticsearchController:
         # ordinarily I'd add a pre-request hook that would remove these parameters,
         # but httpx also does not let the user modify the request before it's sent.
         # (see https://www.python-httpx.org/compatibility/#event-hooks)
-        return {k:v for k,v in params.items() if v is not None}
+        return {k: v for k, v in params.items() if v is not None}
