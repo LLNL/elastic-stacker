ee206ceca7f43b7e0e2adec982308629ca19c085
James Taliaferro <taliaferro1@llnl.gov>
2023-06-06 20:13:34 -0700

    corrected the name of the param cleaning function

--- tools/elastic_stacker/elasticsearch/generic.py
+++ tools/elastic_stacker/elasticsearch/generic.py
@@ -6,10 +6,10 @@ class GenericElasticsearchController:
     def __init__(self, client: httpx.Client):
         self.client = client
 
-    def _clean_headers(self, headers: dict):
+    def _clean_params(self, params: dict):
         # httpx includes query parameters even if their value is None
         # (see https://www.python-httpx.org/compatibility/#query-parameters).
         # ordinarily I'd add a pre-request hook that would remove these parameters,
         # but httpx also does not let the user modify the request before it's sent.
         # (see https://www.python-httpx.org/compatibility/#event-hooks)
-        return {k:v for k,v in headers.items() if v is not None}
+        return {k:v for k,v in params.items() if v is not None}
