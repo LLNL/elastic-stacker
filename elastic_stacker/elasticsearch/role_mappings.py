ee27639ee4b338f10de39e4b8bf5e3d883c9c9ad
James Taliaferro <taliaferro1@llnl.gov>
2023-07-18 16:54:08 -0700

    First pass at role/mapping management, needs debug

---
+++ elastic_stacker/elasticsearch/role_mappings.py
@@ -0,0 +1,89 @@
+import logging
+import os
+
+from httpx import HTTPStatusError
+
+from elastic_stacker.utils.controller import ElasticsearchAPIController
+
+logger = logging.getLogger("elastic_stacker")
+
+
+class RoleMappingController(ElasticsearchAPIController):
+    """
+    RoleMappingController manages the import/export of security role mappings.
+    https://www.elastic.co/guide/en/elasticsearch/reference/current/security-api.html#security-role-mapping-apis
+    """
+
+    _base_endpoint = "/_security/role_mapping/"
+    _resource_directory = "role_mappings"
+
+    def _build_endpoint(self, name: str = "") -> str:
+        return base_endpoint + name
+
+    def get(self, name: str = None) -> dict:
+        """
+        Get one or all of the role mappings on the system.
+        https://www.elastic.co/guide/en/elasticsearch/reference/current/security-api-get-role-mapping.html
+        """
+        endpoint = self._build_endpoint(name)
+        response = self._client.get(endpoint)
+        return response.json()
+
+    def create(
+        self,
+        name: str,
+        role: dict,
+    ):
+        """
+        Create a new role.
+        https://www.elastic.co/guide/en/elasticsearch/reference/current/security-api-put-role_mapping.html
+        """
+        endpoint = self._build_endpoint(name)
+
+        response = self._client.put(
+            endpoint, json=pipeline
+        )
+        return response.json()
+
+    def dump(
+        self,
+        data_directory: os.PathLike = None,
+        **kwargs,
+    ):
+        """
+        Dump all roles on the system to files in the data directory
+        """
+        working_directory = self._get_working_dir(data_directory, create=True)
+        role_mappings = self.get()
+        for name, role_mapping in roles_mappings.items():
+            file_path = working_directory / (name + ".json")
+            self._write_file(file_path, pipeline)
+
+    def load(
+        self,
+        data_directory: os.PathLike = None,
+        delete_after_import: bool = False,
+        allow_failure: bool = False,
+        **kwargs,
+    ):
+        """
+        Read role configurations from files and load them into
+        Elasticsearch.
+        """
+        working_directory = self._get_working_dir(data_directory, create=False)
+
+        for role_mapping_file in working_directory.glob("*.json"):
+            role_mapping = self._read_file(role_mapping_file)
+            role_mapping_name = role_mapping_file.stem
+            try:
+                self.create(role_mapping_name, role_mapping)
+            except HTTPStatusError as e:
+                if allow_failure:
+                    logger.info(
+                        "Experienced an error; continuing because allow_failure is True"
+                    )
+                else:
+                    raise e
+            else:
+                if delete_after_import:
+                    role_mapping_file.unlink()
