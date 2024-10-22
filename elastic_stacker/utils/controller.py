54e1e2ac73b8af982ef6a9c52e5176e343460a3b
James Taliaferro <taliaferro1@llnl.gov>
2024-10-22 11:17:43 -0700

    move key filtering into the controller

--- elastic_stacker/utils/controller.py
+++ elastic_stacker/utils/controller.py
@@ -4,6 +4,50 @@ import re
 
 import httpx
 from pathlib import Path
+from collections import defaultdict
+
+
+def without_keys(d: dict, keys: list[str]):
+    """
+    Returns the given dict d without the specified keys, which may be nested
+    keys separated by dots (e.g. foo.bar.baz). This thin wrapper splits the keys
+    at the dots before passing them down to _without_keys where the real work
+    happens.
+    """
+    keys = { tuple(key.split(".")) for key in keys }
+    return _without_keys(d, keys)
+
+def _without_keys(d: dict, keys: set[tuple[str]] ):
+    """
+    Given a nested dict and a set of keys to delete, selectively walk down the
+    structure, deleting keys as as they're found. Only walks the parts of the
+    tree that are candidates for deletion.
+
+    This is frankly over-engineered, but is about as fast as you can do this;
+    the efficiency comes from never walking a part of the tree more than once.
+    For example; if I want to delete a.b.c.d.e.f and a.b.c.g.h.i, I still walk
+    the tree under a.b.c only once, instead of twice, by first building up a
+    list of keys to delete at each next level of the tree.
+    """
+
+    delete_map = defaultdict(set)
+    for k in keys:
+        if len(k) == 1:
+            # None means this key should be deleted at this level
+            delete_map[k[0]] = None
+        elif delete_map[k[0]] != None:
+            # if k[0] is present at this level, delete k[1:] at the next level
+            delete_map[k[0]].add(k[1:])
+
+    keys = list(d.keys())
+
+    for k in keys:
+        if delete_map[k] is None:
+            del(d[k])
+        elif delete_map[k] and isinstance(d[k], dict):
+            d[k] = _without_keys(d[k], delete_map[k])
+
+    return d
 
 
 def _abs_path(p: os.PathLike):
@@ -43,6 +87,7 @@ class GenericController:
     _resource_directory: str = ""
     _subs: dict
     _touched_files: set[Path]
+    _excluded_attributes: list = []
 
     def __init__(self, client: httpx.Client, subs: dict = {}, **options):
         self._client = client
@@ -61,6 +106,7 @@ class GenericController:
         return value
 
     def _write_file(self, path: os.PathLike, obj: dict):
+        obj = without_keys(obj, self._excluded_attributes)
         path = _abs_path(path)
         output = json.dumps(obj, indent=4, sort_keys=True)
         output = self._run_substitutions(output)
