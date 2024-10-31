25de5e8ae682d4656a6c7a2e5f96649c3302698b
James Taliaferro <taliaferro1@llnl.gov>
2024-10-30 17:46:15 -0700

    fix the __version__ field

--- elastic_stacker/__init__.py
+++ elastic_stacker/__init__.py
@@ -3,7 +3,8 @@ from importlib.metadata import version
 import fire
 from .stacker import Stacker
 
-__version__ = "0.3.1"
+__version__ = version("elastic-stacker")
+
 
 def main():
     fire.Fire(Stacker)
